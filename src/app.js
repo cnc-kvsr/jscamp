3739d0435e007fc017fb0dc3d0655f0a0f876214
kevser canca <kevsercanca@gmail.com>
2021-10-31 18:15:21 +0300

    31.10.2021

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("Merhaba Kodlama.io")
+
+//JS type safe değildir!!!!
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+dolarDun = "9.20"
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11.3
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","Kamu konut kredisi","Özel konut kredisi"]
+
+console.log("</ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>" +konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
+
